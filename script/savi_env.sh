8462725c215d72d6e7eec5b2256c3a4ae303f73e
Hesam Rahimi <hes.rahimi@savinetwork.ca>
2012-07-09 11:16:59 -0400

    Adding scripts for running APIs in HW client
    Bug:179
    Change-Id: Ie3ee62edb5e749affc25c377a5d1c74ae7274cd2

---
+++ script/savi_env.sh
@@ -0,0 +1,11 @@
+# Copyright (c) 2012, The SAVI Project.
+# Eliot J. Kang
+#!/bin/bash -f
+#export CONTROL_WSDL=http://control0.vani.utoronto.ca:8080/SAVI/ControlService?wsdl
+export CONTROL_WSDL=http://localhost:9080/ws/ControlService?wsdl
+export SAVI_HW_JAR=../dist/college-0.1.jar
+export SAVI_USER=summer10
+export SAVI_PASSWORD=summer10
+export SAVI_PROJECT=service
+export SAVI_LOCATION=Toronto
+export SAVI_NETWORK=summer10network
