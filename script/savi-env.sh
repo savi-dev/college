4b44d3820f5b3d29b9cf50ecd29fd68568ca68f8
guest <guest@savinetwork.ca>
2012-07-18 14:28:54 -0400

    Change all scripts for supporting environment variables.
    Bug: 194
    Change-Id: I53212eb60ada958f18b0a1b9f35502e61cd0c7e2

--- script/savi-env.sh
+++ script/savi-env.sh
@@ -1,11 +1,10 @@
+#!/bin/bash -f
 # Copyright (c) 2012, The SAVI Project.
 # Eliot J. Kang
-#!/bin/bash -f
-#export CONTROL_WSDL=http://control0.vani.utoronto.ca:8080/SAVI/ControlService?wsdl
 export CONTROL_WSDL=http://localhost:9080/ws/ControlService?wsdl
 export SAVI_HW_JAR=../dist/college-0.1.jar
-export SAVI_USER=summer10
-export SAVI_PASSWORD=summer10
-export SAVI_PROJECT=service
+export SAVI_USER=demo
+export SAVI_PASSWORD=savi
+export SAVI_PROJECT=demo
 export SAVI_LOCATION=Toronto
 export SAVI_NETWORK=summer10network
